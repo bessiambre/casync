a0dff6d1ba8bd4102fb22e867588e91a1fc4a629
Benoit Essiambre <essiambreb@dplwireless.com>
2020-04-27 11:06:19 -0300

    strict mode

--- casync.js
+++ casync.js
@@ -1,8 +1,9 @@
 "use strict";
-exports.casync=function(fn) {
+exports.casync=function(fn,strict=true) {
     return function(){
 		let args = Array.prototype.slice.call(arguments);
 		let doneCalled=false;
+		let doneAutoCalled=false;
 		let done=null;
 		let next=(err,...cargs)=>{
 			if(err){
@@ -10,9 +11,10 @@ exports.casync=function(fn) {
 			}
 			try{
 				let v=gen.next((cargs.length === 0?undefined:(cargs.length===1?cargs[0]:cargs)));
-				// if(v.done && !doneCalled){
-				// 	done(null);//call done if the function reaches the end. (a but like a normal function returns at the end even if return is not explicitely called)
-				// }
+				if(strict && v.done && !doneCalled){
+					doneAutoCalled=true;
+					done(null);//call done if the function reaches the end. (a bit like a normal function returns at the end even if return is not explicitely called)
+				}
 			}catch(err){
 				if(done!==null){
 					done(err);
@@ -25,7 +27,7 @@ exports.casync=function(fn) {
 			let oldDone=args[args.length-1];
 			done=function(){
 				if(doneCalled){
-					throw new Error("Done called more than once.");
+					throw new Error(`Done called more than once.${(doneAutoCalled?"Done was called automatically when casync-ed function reached the end. If you don't want this behavior you may pass strict=false as the second parameter of casync(fn,strict)":'')}`);
 				}else{
 					doneCalled=true;
 					oldDone.apply(this, arguments);
